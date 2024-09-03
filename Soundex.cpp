ea663eb01db34209a796d1bf27e74310ca6be594
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-09-03 12:23:12 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -6,12 +6,12 @@ char getSoundexCode(char c) {
                                 '1', '2', '6', '2', '3', '0', '0', '0', '2', '0', '2' };
     
     c = toupper(c);
-return(checkTable[(int(c) - 65)];
+return(checkTable[(int(c) - 65)]);
     
 }
 void codeConcatinate(std::string& soundex, char c, char& prevCode)
 {
-    char code = getSoundexCode(name[i]);
+    char code = getSoundexCode(c);
         if (code != '0' && code != prevCode) {
             soundex += code;
             prevCode = code;
@@ -29,7 +29,7 @@ std::string generateSoundex(const std::string& name) {
 
     std::string soundex(1, toupper(name[0]));
     codeCreation(soundex,name);
-    while (soundex.length() < 4) {
+    while (soundex.length() > 4) {
         soundex += '0';
     }
 
