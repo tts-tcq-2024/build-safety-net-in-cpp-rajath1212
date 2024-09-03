a24dabe9952645b6148410b64e43b7f1504370b0
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-09-03 11:26:08 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -2,16 +2,12 @@
 #include <cctype>
 
 char getSoundexCode(char c) {
+    const char checkTable[26]={'0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', 
+                                '1', '2', '6', '2', '3', '0', '0', '0', '2', '0', '2' };
+    
     c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
+return(checkTable[(int(c) - 65)];
+    
 }
 
 std::string generateSoundex(const std::string& name) {
