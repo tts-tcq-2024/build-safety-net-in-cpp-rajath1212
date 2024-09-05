e17e646a9cc32c539c36e1ca2e57deae0e9f4113
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-09-05 09:46:49 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -9,4 +9,16 @@ TEST(SoundexTest, HandlesEmptyString) {
 TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
-
+TEST(SoundexTest, RandomWords) {
+    EXPECT_EQ(generateSoundex("rajath"), "R230");
+    EXPECT_EQ(generateSoundex("Projects"), "P622");
+ 
+}
+TEST(SoundexTest, CaseDifferenceWords) {
+    EXPECT_EQ(generateSoundex("GiThUb"), "G310");   
+    EXPECT_EQ(generateSoundex("BoSCh"), "B200");   
+}
+TEST(SoundexTest, SpecialCharecters) {
+    EXPECT_EQ(generateSoundex("R@nDom"), "G310");   
+    EXPECT_EQ(generateSoundex("CLa#"), "B200");   
+}
